bf58e7b188f69fdeb5d62dd1a3791ba8b86303a8
infant <y.infant1996@gmail.com>
2022-11-14 15:14:15 +0530

    done

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connect = (url) => {
   return mongoose
     .connect(url)
-    .then(() => console.log(`Connection Established!😁`))
+    .then(() => console.log(`Connection Established!`))
     .catch((error) => console.log(`Connection Error ${error}`));
 };
 
